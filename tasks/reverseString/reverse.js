0d9efdc6c3dfcce5fd7ee39ee35e20b3e87e93ed
shayshahal <shay840@gmail.com>
2023-01-06 00:19:46 +0200

    finish reverse

---
+++ tasks/reverseString/reverse.js
@@ -0,0 +1,7 @@
+export default function reverse(str)
+{
+    let ans = '';
+    for(let i = str.length-1; i>=0; i--)
+        ans += str[i]
+    return ans;
+}
--- tasks/reverseString/reverse.test.js
+++ tasks/reverseString/reverse.test.js
@@ -1,3 +1,26 @@
-import './reverse'
+import reverse from './reverse'
 
-test('', ()=>{})
+test('normal string', ()=>{
+    expect(
+        reverse('Shay Shahal')
+        ).toBe('lahahS yahS');
+})
+
+test('special characters', ()=>{
+    expect(
+        reverse('!@#$%')
+    ).toBe('%$#@!');
+})
+
+test('racecar', ()=>
+{
+    expect(
+        reverse('racecar')
+    ).toBe('racecar')
+});
+test('using js', ()=>
+{
+    expect(
+        reverse('javascript')
+    ).toBe('javascript'.split('').reverse().join(''))
+})
