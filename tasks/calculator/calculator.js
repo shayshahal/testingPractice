e6fd38b46623aae09407ff02abdab871df5df69f
shayshahal <shay840@gmail.com>
2023-01-06 01:03:08 +0200

    finished calculator

---
+++ tasks/calculator/calculator.js
@@ -0,0 +1,9 @@
+const calculator = 
+{
+    add : (a,b) => a+b,
+    sub: (a,b)=> a-b,
+    multiply: (a,b)=> a*b,
+    divide: (a,b)=> a/b
+}
+
+export default calculator;
--- tasks/calculator/calculator.test.js
+++ tasks/calculator/calculator.test.js
@@ -1,3 +1,37 @@
-import './calculator'
+import calculator from './calculator'
 
-test('', ()=>{})
+test('add', ()=>{
+    let a = ~~(Math.random()*10), b = ~~(Math.random()*10);
+    expect(
+        calculator.add(a, b)
+    ).toBe(a + b);
+})
+test('sub', ()=>{
+    let a = ~~(Math.random()*10), b = ~~(Math.random()*10);
+    expect(
+        calculator.sub(a, b)
+    ).toBe(a - b);
+})
+test('multiply', ()=>{
+    let a = ~~(Math.random()*10), b = ~~(Math.random()*10);
+    expect(
+        calculator.multiply(a, b)
+    ).toBe(a * b);
+})
+test('divide normal', ()=>{
+    let a = Math.random()*10+1, b = Math.random()*5 + 1, c = a*b;
+    expect(
+        calculator.divide(c, a)
+    ).toBe(b);
+})
+test('divide floating', ()=>{
+    let a = Math.random()*10+1, b = Math.random(), c = a*b;
+    expect(
+        calculator.divide(c, a)
+    ).toBeCloseTo(b);
+})
+test('divide by zero', ()=>{
+    expect(
+        calculator.divide(4, 0)
+    ).toBe(Infinity);
+})
